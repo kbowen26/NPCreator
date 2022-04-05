0247cb7b5b538f41567639f259fceebfe518bcd8
kbowen26 <kbowen26@uncc.edu>
2022-04-05 14:57:30 -0400

    first commit

---
+++ app/src/main/java/com/itis_4440/npcreator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.itis_4440.npcreator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
