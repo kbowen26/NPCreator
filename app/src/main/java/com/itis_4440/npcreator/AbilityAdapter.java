99f2a415602ecc0fc3a194115c92ffa9cc55be31
kbowen26 <kbowen26@uncc.edu>
2022-04-09 17:42:05 -0400

    Create empty Ability/Action adapters for recyclerviews, added frontend according to specifications

---
+++ app/src/main/java/com/itis_4440/npcreator/AbilityAdapter.java
@@ -0,0 +1,5 @@
+package com.itis_4440.npcreator;
+
+public class AbilityAdapter {
+    //TODO FILL OUT ABILITY ADAPTER
+}
---
+++ app/src/main/java/com/itis_4440/npcreator/ActionAdapter.java
@@ -0,0 +1,5 @@
+package com.itis_4440.npcreator;
+
+public class ActionAdapter {
+    //TODO FILL OUT ACTION ADAPTER
+}
